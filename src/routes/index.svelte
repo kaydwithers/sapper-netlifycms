037263450fd647698e3ab7d5328c7dd38225333f
Kayd Withers <kayd.withers@isobar.com>
2020-03-02 20:41:58 +1100

    Configured the site to run the Netlify CMS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -34,6 +34,7 @@
 
 <svelte:head>
 	<title>Sapper project template</title>
+	<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 </svelte:head>
 
 <h1>Great success!</h1>
@@ -44,3 +45,19 @@
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  });
+</script>
